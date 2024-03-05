d201f3eaca516e40644cfc3d0673da279541e02d
28530367 <h.james1011@gmail.com>
2024-03-05 21:51:24 +0800

    HumanMine GO terms

---
+++ cancerAnalysis/humanmine_GO-terms_arrangement.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+
+# 取得當前工作目錄
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+def readCSV(file_name):
+    filePath = f"{current_directory}/{file_name}"
+    fileData_df = pd.read_csv(filePath)
+
+    return fileData_df
+
+def outputCSV(output_df, file_name):
+    filePath = f"{current_directory}/{file_name}"
+    output_df.to_csv(filePath, index=False)
+    print('CSV SUCCESS!!')
+
+if __name__ == "__main__":
+    readData_df = readCSV('humanmine_GO-terms_AllData.csv')
+    
+    column_rename_dict = {'Gene.primaryIdentifier': 'Gene Primary Identifier',
+                          'Gene.symbol': 'Gene Symbol',
+                          'Gene.name': 'Gene Name',
+                          'Gene.goAnnotation.ontologyTerm.identifier': 'Ontology Term Identifier',
+                          'Gene.goAnnotation.ontologyTerm.name': 'Ontology Term Name',
+                          'Gene.organism.shortName': 'Organism Short Name',
+                          'Gene.goAnnotation.ontologyTerm.parents.name': 'Parents Name'
+                          }
+    
+    data_df = readData_df.rename(columns=column_rename_dict)
+
+    # groupby
+    quantity_df = data_df.groupby('Parents Name').size().reset_index(name='Quantity')
+    arrangement_df = data_df.groupby('Parents Name').agg({'Gene Symbol': ', '.join}).reset_index()
+    
+    # merge
+    arrangement_df = arrangement_df.merge(quantity_df, on='Parents Name')
+
+    target_column_list = ['Parents Name', 'Quantity', 'Gene Symbol']
+    arrangement_df = arrangement_df[target_column_list]
+
+    # 輸出csv
+    outputCSV(arrangement_df, 'humanmine_GO-terms_arrangement.csv')
